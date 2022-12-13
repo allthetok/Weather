da3e442c84070e66094aa3e3e04d02802df29d38
allthetok <al.tokjuman@gmail.com>
2022-12-12 20:18:07 -0500

    toggle state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Geocode from 'react-geocode'
 import axios from 'axios'
+import suncalc from 'suncalc'
 import { Card } from 'react-bootstrap'
 
 const weatherCode = {
@@ -32,12 +34,32 @@ const weatherCode = {
   '7102': 'Light Ice Pellets',
   '8000': 'Thunderstorm'
 }
+
+const Timer = (startingTime) => {
+  const [clock, setClock] = useState(startingTime)
+  // useEffect(() => {
+  //   if (startingTime !== null) {
+  //     setTimeout(() => {
+  //       let timeStamp = new Date(startingTime)
+  //       timeStamp.setSeconds(timeStamp.getSeconds() + 1)
+  //       setClock(timeStamp.toLocaleTimeString())
+  //       //setTime(timeStamp.toLocaleTimeString())
+  //     }, 1000)
+  //   }
+  // },[])
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
 const App = () => {
 
   const [inputCity, setInputCity] = useState('')
-  const [weather, setWeather] = useState({ degree: '', appdegree: '', condition: '' })
+  const [weather, setWeather] = useState({ degree: '', appdegree: '', condition: '', sunrise: '', sunset: '' })
   const [name, setName] = useState('')
-  const [time, setTime] = useState()
+  const [time, setTime] = useState('')
+  const [showDet, setShowDet] = useState()
 
   Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY)
   Geocode.setLanguage('en')
@@ -54,14 +76,15 @@ const App = () => {
     )
   }
 
-
   const getWeatherData = async (response) => {
     const { lat, lng } = response.results[0].geometry.location
     await axios.get(`https://api.tomorrow.io/v4/timelines?location=${lat},${lng}&fields=temperature,temperatureApparent,weatherCode&timesteps=1h&units=metric&apikey=${process.env.REACT_APP_TOMORROW_API_KEY}`)
       .then((res) => {
         let newWeather = { ...weather, degree: res.data.data.timelines[0].intervals[0].values.temperature,
           appdegree: res.data.data.timelines[0].intervals[0].values.temperatureApparent,
-          condition: res.data.data.timelines[0].intervals[0].values.weatherCode  }
+          condition: res.data.data.timelines[0].intervals[0].values.weatherCode,
+          sunrise: getDuskAndDawn(lat, lng, 'sunrise'),
+          sunset: getDuskAndDawn(lat, lng, 'sunset') }
         setWeather(newWeather)
       })
     Geocode.fromLatLng(lat, lng)
@@ -91,6 +114,7 @@ const App = () => {
         getCurrentTime()
       })
 
+
   }
 
   const condNumToString = (code) => {
@@ -103,19 +127,30 @@ const App = () => {
       axios.get(`http://api.timezonedb.com/v2.1/get-time-zone?key=${process.env.REACT_APP_TIMEZONEDB_API_KEY}&format=json&by=position&lat=${lat}&lng=${lng}`)
         .then((res) => {
           console.log(res.data.formatted)
-          let timeStamp = new Date(res.data.formatted)
-          console.log(timeStamp.getMinutes() + ':' + timeStamp.getSeconds())
-          timeStamp.setSeconds(timeStamp.getSeconds() + 1)
-          console.log(timeStamp.toLocaleTimeString())
-          setTime(timeStamp.toLocaleTimeString())
+          console.log(typeof(res.data.formatted))
+          const start = res.data.timestamp
+          setTime(start)
+          // let timeStamp = new Date(res.data.formatted)
+          // console.log(timeStamp.getMinutes() + ':' + timeStamp.getSeconds())
+          // timeStamp.setSeconds(timeStamp.getSeconds() + 1)
+          // console.log(timeStamp.toLocaleTimeString())
+          // setTime(timeStamp.toLocaleTimeString())
+          // setTime(res.data.formatted)
         })
     })
 
   }
 
-  const getAssetPath = (code) => {
-    return `/color/${code}.svg`
+  const getAssetPath = (code) => `/color/${code}.svg`
+
+  const getDuskAndDawn = (lat, lng, pref) => {
+    const suncalcObj = suncalc.getTimes(new Date(), lat, lng)
+    if (pref === 'sunrise') {
+      return `${suncalcObj.sunrise.getHours()}:${suncalcObj.sunrise.getMinutes()}`
+    }
+    return `${suncalcObj.sunset.getHours()}:${suncalcObj.sunset.getMinutes()}`
   }
+
 
   return (
     <div className='background'>
@@ -130,16 +165,19 @@ const App = () => {
 
         {weather.degree !== '' &&
       <>
-        {/* <p>Current Temperature in {name} : {Math.floor(weather.degree)}°C and {condNumToString(weather.condition)}. It feels like {Math.floor(weather.appdegree)}°C.</p>
-        <img src='../assets/small/png/' /> */}
-        <Card style={{ width: '30rem', margin: 'auto' }}>
-          <img className='weather-code' src={getAssetPath(weather.condition)}/>
+        <Card style={{ width: '30rem', margin: 'auto', display: 'block' }}>
+          <p className='inline degree'>{Math.floor(weather.degree)} °C</p>
+          <img className='inline weather-code' src={getAssetPath(weather.condition)}/>
           <Card.Body>
             <Card.Title>{name}</Card.Title>
+            <button className='moreinfo' onClick={() => setShowDet(!showDet)}>More Info</button>
+            {showDet &&
             <Card.Text>
                 The current temperature in {name} is {Math.floor(weather.degree)} °C and {condNumToString(weather.condition)}. It feels like {Math.floor(weather.appdegree)}°C.
-              {time}
+                The sunrise is at {weather.sunrise}, the sunset is at {weather.sunset} {time}
+              <Timer startingTime={time}/>
             </Card.Text>
+            }
           </Card.Body>
         </Card>
       </>
